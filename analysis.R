64e59683073899b048629c4868aadfdc03ef71d8
niehusst <liamniehusstaab@gmail.com>
2018-12-11 15:50:16 -0600

    added tag analysis

--- analysis.R
+++ analysis.R
@@ -7,6 +7,9 @@
 #  - Answers of which tag are the most successful? The least successful?
 #  - Do higher answer scores and answer acceptance correlate?
 
+library(data.table)
+
+
 # raw data (for simplicity with early questions)
 answers = read.csv("~/Downloads/rquestions/answers.csv")
 questions = read.csv("~/Downloads/rquestions/questions.csv")
@@ -30,8 +33,25 @@ totalAcceptedAnswers = sum(answers$IsAcceptedAnswer == TRUE)
 percentAccepted = as.double(100/totalQuestions) * as.double(totalAcceptedAnswers)
 sprintf("The percentage of answers accepted in this data is %.5f%% of %i total questions.", percentAccepted, totalQuestions)
 
-### do higher answer scores correlate with asker acceptance
+### what are the most popular tags on questions with accepted answers?
+Tags = data.table(tags)
+Tags = Tags[order(Count, decreasing = TRUE)]
+head(Tags)
+# The most popular tags are the basic ones. It makes sense that simpler questions
+#that are asked more often will have more accepted answers, so it is logical to
+#assume that these most popular tags will also be most popular on questions with 
+#accepted answers.
+
+### answers of which tag are the most successful? The least successful?
+# Considering easier questions pertaining to the basic tags (which are popular on
+#their own), I find it unlikely that this new question would reveal any new 
+#information. 
+
+### do higher answer scores correlate with asker acceptance?
 #create a model to examine correlation between these variables
 #limit data to only accepted answers for analysis
 #(but to account for less popular/visited questions, score should be a percentage out of
 # highest score for an answer to that question)
+
+### do images/code/links being included correlate with asker acceptance?
+

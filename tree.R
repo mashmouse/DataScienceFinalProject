68fd3bfded9147544dd3b411446916fd4990ec3c
Yash Gupta <yashdavisgupta@github.com>
2018-12-11 14:57:26 -0600

    tree gets ALL TEH VARIABLES

--- tree.R
+++ tree.R
@@ -3,7 +3,7 @@
 library(tree)
 
 #Create Tree
-tree1 <- tree(IsAcceptedAnswer~qa$ContainsImage + qa$ContainsCode + qa$AnswerScore + qa$ContainsLink + qa$ResponseTimeHours, data=qa)
+tree1 <- tree(IsAcceptedAnswer~qa$ContainsImage + qa$ContainsCode + qa$AnswerScore + qa$ContainsLink + qa$ResponseTimeHours + qa$AnswerBodyTextLength + qa$QAwordIntersection, data=qa)
 #Print Tree
 plot(tree1)
 text(tree1)

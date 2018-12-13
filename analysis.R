2b9fb639bb01ce9880884b43b0fc5f3d3f58e3b9
niehusst <liamniehusstaab@gmail.com>
2018-12-12 19:49:11 -0600

    added correlation data. Doesn't look good

--- analysis.R
+++ analysis.R
@@ -8,7 +8,7 @@
 #  - Do higher answer scores and answer acceptance correlate?
 
 library(data.table)
-
+library(ggplot2)
 
 # raw data (for simplicity with early questions)
 answers = read.csv("~/Downloads/rquestions/answers.csv")
@@ -16,6 +16,19 @@ questions = read.csv("~/Downloads/rquestions/questions.csv")
 # upload the cleaned csv R questions data
 qa = read.csv("~/Desktop/Data\ Science/DataScienceFinalProject/cleanData.csv")
 
+### what are the most popular tags on questions with accepted answers?
+Tags = data.table(tags)
+Tags = Tags[order(Count, decreasing = TRUE)]
+head(Tags)
+# The most popular tags are the basic ones. It makes sense that simpler questions
+#that are asked more often will have more accepted answers, so it is logical to
+#assume that these most popular tags will also be most popular on questions with 
+#accepted answers.
+
+### answers of which tag are the most successful? The least successful?
+# Considering easier questions pertaining to the basic tags (which are popular on
+#their own), I find it unlikely that this new question would reveal any new 
+#information. 
 
 ### percentage answers upvoted
 #Due to the nature of stack overflow, answers can have a negative score, so we will 
@@ -33,25 +46,46 @@ totalAcceptedAnswers = sum(answers$IsAcceptedAnswer == TRUE)
 percentAccepted = as.double(100/totalQuestions) * as.double(totalAcceptedAnswers)
 sprintf("The percentage of answers accepted in this data is %.5f%% of %i total questions.", percentAccepted, totalQuestions)
 
-### what are the most popular tags on questions with accepted answers?
-Tags = data.table(tags)
-Tags = Tags[order(Count, decreasing = TRUE)]
-head(Tags)
-# The most popular tags are the basic ones. It makes sense that simpler questions
-#that are asked more often will have more accepted answers, so it is logical to
-#assume that these most popular tags will also be most popular on questions with 
-#accepted answers.
+### average score of accepted answer
+acceptedAnswers = filter(answers, IsAcceptedAnswer == TRUE)
+avg = sum(acceptedAnswers$AnswerScore) / nrow(acceptedAnswers)
+sprintf("The mean score of accepted answers is %.4f.", avg)
+#plot variance
+filteredAnswers = filter(answers, AnswerScore < 30)
+filteredAnswers$IsAcceptedAnswer[filteredAnswers$IsAcceptedAnswer==TRUE] <- "Accepted" #adjust labels
+filteredAnswers$IsAcceptedAnswer[filteredAnswers$IsAcceptedAnswer==FALSE]   <- "Not Accepted"
+ggplot( data = filteredAnswers, aes(x = AnswerScore)) +
+  geom_histogram(binwidth = 1) + 
+  facet_wrap(~IsAcceptedAnswer) + 
+  labs(title = "Histogram of Answer Score",
+       x = "Answer Score",
+       y = "Number of Answers") 
 
-### answers of which tag are the most successful? The least successful?
-# Considering easier questions pertaining to the basic tags (which are popular on
-#their own), I find it unlikely that this new question would reveal any new 
-#information. 
+### average score of unaccepted answer
+unacceptedAnswers = filter(answers, IsAcceptedAnswer == FALSE)
+avg = sum(unacceptedAnswers$AnswerScore) / nrow(unacceptedAnswers)
+sprintf("The mean score of not accepted answers is %.4f.", avg)
+
+### number of accepted answers with a score of 0 or less
+acceptedLowScore = (100/nrow(acceptedAnswers)) * sum(answers$IsAcceptedAnswer == TRUE & answers$AnswerScore <= 0)
+sprintf("The percent of accepted answers with a score of 0 or less is %.2f%%.", acceptedLowScore)
+#185 accepted answers had a score of less than 0
 
 ### do higher answer scores correlate with asker acceptance?
-#create a model to examine correlation between these variables
 #limit data to only accepted answers for analysis
-#(but to account for less popular/visited questions, score should be a percentage out of
-# highest score for an answer to that question)
+acceptedAnswers = filter(answers, IsAcceptedAnswer == TRUE)
+#Use Pearson's correlation to examine correlation between AnswerScore and IsAcceptedAnswer
+cor.test(answers$AnswerScore, as.numeric(answers$IsAcceptedAnswer))
+#correlation is 0.0828. So not a very good linear correlation
+#(try to account for less popular/visited questions, score should be a percentage out of
+# highest score for an answer to that question)?
 
-### do images/code/links being included correlate with asker acceptance?
+
+### do images/code/links being included in answers correlate with asker acceptance?
+cor.test(as.numeric(qa$ContainsImage), as.numeric(qa$IsAcceptedAnswer))
+#.091
+cor.test(as.numeric(qa$ContainsCode), as.numeric(qa$IsAcceptedAnswer))
+#.103
+cor.test(as.numeric(qa$ContainsLink), as.numeric(qa$IsAcceptedAnswer))
+#.015
 
